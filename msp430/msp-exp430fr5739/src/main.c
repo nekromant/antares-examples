4a50f11666173ed01f6fa99575bed10bfa16559b
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2012-09-05 21:58:16 +0400

    Add msp430/msp-exp430fr5739 example
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

---
+++ msp430/msp-exp430fr5739/src/main.c
@@ -0,0 +1,53 @@
+#include <arch/antares.h>
+#include <msp430.h>
+#include <arch/lib/delaylib.h>
+
+
+ANTARES_INIT_LOW(wdt_dsbl)
+{
+	WDTCTL = WDTPW | WDTHOLD;	
+}
+
+ANTARES_INIT_LOW(led_init)
+{
+	PJDIR |= 0x0f;
+	P3DIR |= 0xf0;
+	P3OUT |= 0xf0;
+}
+
+void frexp_leds(char leds)
+{
+	/* Reset bits */
+	P3OUT&= ~0xf0;
+	PJOUT&= ~0x0f;
+	/* Write state */
+	PJOUT |= leds & 0x0f;
+	P3OUT |= leds & 0xf0;
+}
+
+/* This 'app' will be called over and over in a loop */
+ANTARES_APP(blinker_app)
+{
+	int i;
+#if defined(CONFIG_LED_LEFT) || defined(CONFIG_LED_KITT)
+	for (i=0; i<8; i++) {
+		frexp_leds( 1<<i );
+		delay_s(CONFIG_LED_DELAY);
+	}
+#endif
+#if defined(CONFIG_LED_RIGHT) || defined(CONFIG_LED_KITT)
+	for (i=7; i>=0; i--) {
+		frexp_leds( 1<<i );
+		delay_s(CONFIG_LED_DELAY);
+	}
+#endif
+
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+        while (!(P1IN & (1<<3)));; /* debounce */
+	P1IFG &= ~(1<<3); /* Clear the isr flag */
+	P1OUT^=1<<6;
+}
