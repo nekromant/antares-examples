2e5e7f76248f073356d441b9ea8eb683ae3060be
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2012-09-06 20:46:30 +0400

    avr: Add usb t2313 blinky example
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

---
+++ avr/2313-blinky/src/test.c
@@ -0,0 +1,17 @@
+#include <arch/antares.h> 
+#include <avr/io.h> 
+#include <util/delay.h>  
+
+ANTARES_INIT_LOW(io_init) 
+{ 
+  DDRD=0xff; 
+}
+
+ANTARES_APP(blink) 
+{ 
+  while(1)
+  {
+    PORTD^=0xff; 
+    _delay_ms(1000);
+  }   
+}
